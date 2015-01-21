70c1f3b8ab4e2783407bd418da8230b803d15053
Cdingram <cdingram@ualberta.ca>
2015-01-20 19:38:56 -0700

    Fixed save features, added data persistance, started clear functionality

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,16 +1,19 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -23,6 +26,8 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayAdapter<String> adapter;
+	private ArrayList<String> tweets;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -33,14 +38,30 @@ public class LonelyTwitterActivity extends Activity {
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+		Button clearButton = (Button) findViewById(R.id.clear);
+		
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				tweets.removeAll(tweets);
+				adapter.notifyDataSetChanged();
+				
+			}
+		});
+		
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
+				if (text.equals("")) {
+					return;
+				}
 				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				tweets.add(text);
+				adapter.notifyDataSetChanged();
+				//finish();
 
 			}
 		});
@@ -67,22 +88,28 @@ public class LonelyTwitterActivity extends Activity {
 		
 		
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		tweets = loadFromFile();
+		
+		if(tweets == null){
+			tweets = new ArrayList<String>();
+		}
+		
+		adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
+	private ArrayList<String> loadFromFile() {
+		Gson gson = new Gson();
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+			InputStreamReader in = new InputStreamReader(fis);
+			// Taken From http://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/index.html
+			Type typeOfT = new TypeToken<ArrayList<String>>(){}.getType();
+			tweets = gson.fromJson(in, typeOfT);
+			fis.close();
+			
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -91,16 +118,19 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		return tweets;
 	}
 	
 	private void saveInFile(String text, Date date) {
+		Gson gson = new Gson();
+		
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+			FileOutputStream fos = openFileOutput(FILENAME, 0);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			gson.toJson(tweets, osw);
+			osw.flush();
 			fos.close();
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
